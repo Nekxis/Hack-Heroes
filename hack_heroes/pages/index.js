24c8b8cb5b0992642887000806cb3903c3bc1036
Nekxis <nexispolska16@gmail.com>
2022-10-11 22:03:55 +0200

    a

--- hack_heroes/pages/index.js
+++ hack_heroes/pages/index.js
@@ -1,8 +1,10 @@
 import Navbar from '../src/components/Navbar/Navbar';
+import MainSection from "../src/components/MainSection/MainSection";
 export default function Home() {
   return (
     <div>
         <Navbar />
+        <MainSection />
     </div>
   )
 }
