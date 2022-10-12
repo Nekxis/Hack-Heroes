2a6e9489a1badf4701ace23506b48e0fa149f941
Nekxis <nexispolska16@gmail.com>
2022-10-12 14:43:55 +0200

    css login

--- hack_heroes/src/components/Auth/Register.js
+++ hack_heroes/src/components/Auth/Register.js
@@ -1,27 +1,27 @@
-const Register = () => { 
+const Register = () => {
     return(
-        < div className="grid grid-cols-1 sm:grid-cols-1 h-screen w-full"> 
-            <div className="bg-White flex flex-col justify-center" >
-                <form className="max-w-[400] w-1/3 mx-auto bg-gray p-14 px-20 rounded-lg ">
-                    <h2 className="text-4x1 dark:text-black font-semibold text-center py-6 text-5xl">Register</h2>
-                    <div className="flex flex-col text-black  py-4">
-                        <label>Username</label>
-                        <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-black focus:border-gray" type="email"/>
+      < div className="grid grid-cols-1 sm:grid-cols-1 h-screen w-full">
+        <div className="bg-white flex flex-col justify-center">
+          <form className="md:max-w-sm sm:max-w-xs drop-shadow-2xl bg-whiteGray mx-auto border-black border-2 p-20 sm:p-14 px-20 rounded-lg">
+                    <h2 className="text-black font-normal text-center text-4xl">Register</h2>
+                    <div className="flex flex-col text-gray-400 py-4">
+                      <label className={'font-bold'}>Username</label>
+                      <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="text"/>
                     </div>
-                    <div className="flex flex-col text-black  py-4">
-                        <label>Email</label>
-                        <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2 border-black focus:border-gray" type="email"/>
+                    <div className="flex flex-col  text-gray-400 py-6">
+                      <label className={'font-bold'}>Email</label>
+                      <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="email"/>
                     </div>
-                    <div className="flex flex-col text-black py-4">
-                        <label>Password</label>
-                        <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2 border-black focus:border-gray" type="text"/>
+                    <div className="flex flex-col text-gray-400 py-4">
+                      <label className={'font-bold'}>Password</label>
+                      <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="text"/>
                     </div>
-                    <div className="flex flex-col text-black py-4">
-                        <label>Repeat password</label>
-                        <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2 border-black focus:border-gray" type="text"/>
+                    <div className="flex flex-col text-gray-400 py-4">
+                      <label className={'font-bold'}>Repeat Password</label>
+                      <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="text"/>
                     </div>
                     <div className="w-full flex flex-col items-center">
-                    <button className="w-1/2 justify-center m-auto content-center text-white my-8 py-3 bg-black rounded-lg">Register</button>
+                      <button className="w-1/2 justify-center drop-shadow-xl m-auto content-center text-white mt-8 py-3 bg-graphite rounded-lg hover:bg-lite-graphite focus:bg-super-lite-graphite">Register</button>
                     </div>
                 </form>
             </div>
