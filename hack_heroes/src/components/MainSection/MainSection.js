149acb84d6d64efc218c1e8ac0ebd1d56681abef
Nekxis <nexispolska16@gmail.com>
2022-10-12 12:45:02 +0200

    beta main

--- hack_heroes/src/components/MainSection/MainSection.js
+++ hack_heroes/src/components/MainSection/MainSection.js
@@ -2,8 +2,16 @@ import SectionElement from "../SectionElement/SectionElement";
 
 export default function MainSection () {
   return(
-    <div className={'w-full flex justify-around'}>
+    <div className={'w-full grid grid-cols-1 justify-around lg:grid-cols-2'}>
       <SectionElement />
+      <SectionElement />
+      <SectionElement />
+      <SectionElement />
+      <SectionElement />
+      <SectionElement />
+      <SectionElement />
+      <SectionElement />
+
     </div>
   )
 }
