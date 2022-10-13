ab3179b23f9001c2d3b0415cd2797a585aa65f53
Nekxis <nexispolska16@gmail.com>
2022-10-13 23:34:14 +0200

    first

--- hack_heroes/pages/index.js
+++ hack_heroes/pages/index.js
@@ -1,10 +1,12 @@
 import Navbar from '../src/components/Navbar/Navbar';
 import MainSection from "../src/components/MainSection/MainSection";
+import Footer from "../src/components/Footer/Fotter";
 export default function Home() {
   return (
     <div>
         <Navbar />
       <MainSection />
+      <Footer />
     </div>
   )
 }
--- hack_heroes/src/components/MainSection/SectionElement/SectionElement.js
+++ hack_heroes/src/components/MainSection/SectionElement/SectionElement.js
@@ -2,11 +2,11 @@ export default function SectionElement () {
   return(
     <div className="flex justify-around bg-white box-border">
       <div className="mx-auto border-solid border-4 border-zinc-600 rounded-md drop-shadow-md hover:shadow-xl hover:bg-white-smoke grid max-w-sm grid-cols-1 justify-end items-center gap-y-16 gap-x-8 py-24 px-4 sm:px-6 sm:py-10 sm:my-7 sm:mx-10 lg:max-w-7xl lg:grid-cols-3 lg:my-6 lg:mx-6 lg:px-8">
-          <div className={'flex lg:justify-end w-full sm:justify-around shadow-md'}>
+          <div className={'flex lg:justify-end sm:justify-around'}>
               <img
                 src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
                 alt="Walnut card tray with white powder coated steel divider and 3 punch out holes."
-                className="flex rounded-lg bg-gray-100  lg:w-full sm:w-1/2 "
+                className="flex rounded-lg bg-gray-100  lg:w-full sm:w-1/2 shadow-md"
               />
         </div>
         <div className={'lg:col-span-2 lg:pl-4 pl-4'}>
