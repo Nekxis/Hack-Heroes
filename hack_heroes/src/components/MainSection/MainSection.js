45077b2211c876d17897f1e1cacbe13a7c7b481e
Nekxis <nexispolska16@gmail.com>
2022-10-14 01:03:50 +0200

    main layout fix

--- hack_heroes/src/components/MainSection/MainSection.js
+++ hack_heroes/src/components/MainSection/MainSection.js
@@ -2,7 +2,11 @@ import SectionElement from "./SectionElement/SectionElement";
 
 export default function MainSection () {
   return(
-    <div className={'w-full grid grid-cols-1 justify-around lg:grid-cols-2'}>
+    <div className={'w-full justify-around grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'}>
+      <SectionElement />
+      <SectionElement />
+      <SectionElement />
+      <SectionElement />
       <SectionElement />
       <SectionElement />
       <SectionElement />
--- hack_heroes/src/components/MainSection/SectionElement/SectionElement.js
+++ hack_heroes/src/components/MainSection/SectionElement/SectionElement.js
@@ -1,7 +1,7 @@
 export default function SectionElement () {
   return(
     <div className="flex justify-around bg-white box-border">
-      <div className="mx-auto border-solid border-4 border-zinc-600 rounded-md drop-shadow-md hover:shadow-xl hover:bg-white-smoke grid max-w-sm grid-cols-1 justify-end items-center gap-y-16 gap-x-8 py-24 px-4 sm:px-6 sm:py-10 sm:my-7 sm:mx-10 lg:max-w-7xl lg:grid-cols-3 lg:my-6 lg:mx-6 lg:px-8">
+      <div className="border-solid border-4 border-zinc-600 rounded-md drop-shadow-md hover:shadow-xl hover:bg-white-smoke grid max-w-sm grid-cols-1 justify-end items-center gap-y-24 gap-x-4 py-14 px-4 sm:px-6 sm:py-10 sm:my-7 sm:mx-10 lg:max-w-7xl lg:grid-cols-3 lg:my-4 lg:mx-6 lg:px-8">
           <div className={'flex lg:justify-end sm:justify-around'}>
               <img
                 src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
