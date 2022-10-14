25a130b3bfab91ebe19f5ecc6e311fb6f2d1de64
Nekxis <nexispolska16@gmail.com>
2022-10-14 23:50:07 +0200

    1

--- hack_heroes/src/components/MyAccount/MyAccount.js
+++ hack_heroes/src/components/MyAccount/MyAccount.js
@@ -1,5 +1,8 @@
 import DataTab from "./Tabs/DataTab"
+import { useState } from "react";
+
 export default function MyAccount () {
+  const [tabs, setTabs] = useState(true);
 
 const profilePhoto = [{
   photo:'https://freepikpsd.com/file/2019/10/default-profile-picture-png-1-Transparent-Images.png'
@@ -10,11 +13,12 @@ const profilePhoto = [{
       <img className="w-1/10 h-1/6 rounded-full mt-10"  src={profilePhoto[0].photo}></img>
       <h2 className="text-xl font-bold">Olaf Wiśnia</h2>
       <div className="grid lg:grid-cols-2 m-8 grid-col-1 ">
-        <button className="w-full px-36 py-4  border-solid border-2  border-gray-400 hover:border-gray-300 active:border-b-black">Moje dane</button>
-        <button className="w-full px-36 py-4  border-solid border-2  border-gray-400 hover:border-gray-300 active:border-b-black">Histioria moich zleceń</button>
+        <button onClick={() => setTabs(true)} className="w-full px-36 py-4  border-solid border-2  border-gray-400 hover:border-gray-300 active:border-b-black">Moje dane</button>
+        <button onClick={() => setTabs(false)} className="w-full px-36 py-4  border-solid border-2  border-gray-400 hover:border-gray-300 active:border-b-black">Histioria moich zleceń</button>
       </div>
       <div className="flex items-center w-full justify-center">
-      <DataTab></DataTab>
+        {tabs && <DataTab />}
+        {/*{!tabs && <HistoryTab />}*/}
       </div>
     </div>
   )
