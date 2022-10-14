e7baf3114600bd560e45753ec3a674a0f4493531
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2022-10-14 23:31:19 +0200

    firebase-config

--- hack_heroes/firebase.js
+++ hack_heroes/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyAl7CsIkn9QwPE7nacyDxRrk5axgq3i6kA",
+    authDomain: "hack-heroes-project.firebaseapp.com",
+    projectId: "hack-heroes-project",
+    storageBucket: "hack-heroes-project.appspot.com",
+    messagingSenderId: "642285336043",
+    appId: "1:642285336043:web:271094e739c8724e2015ae"
 };
 
 
