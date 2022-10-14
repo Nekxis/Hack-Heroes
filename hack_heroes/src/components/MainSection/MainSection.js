abe5e4c1c080e70644883bb1eb185b347f659852
Nekxis <nexispolska16@gmail.com>
2022-10-15 00:19:33 +0200

    grid error

--- hack_heroes/src/components/MainSection/MainSection.js
+++ hack_heroes/src/components/MainSection/MainSection.js
@@ -4,10 +4,10 @@ import SectionElement from "./SectionElement/SectionElement";
 export default function MainSection({ orders }) {
 
 
-  
+
 
   return (
-    <div className={'w-full justify-around grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'}>
+    <div className={'w-full grid gap-2 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'}>
       {orders.map((order) => {
         return (
           <SectionElement key={order.id} id={order.id} title={order.title} description={order.description}  price={order.price}/>
