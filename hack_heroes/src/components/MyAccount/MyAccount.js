4ae38e0388c1e95c5c4aa5a0e80a91f7143a8c68
pawelcp <94795184+pawelcp@users.noreply.github.com>
2022-10-14 23:05:32 +0200

    qwekmqwme

--- hack_heroes/src/components/MyAccount/MyAccount.js
+++ hack_heroes/src/components/MyAccount/MyAccount.js
@@ -1,33 +1,16 @@
 export default function MyAccount () {
+
+const profilePhoto = [{
+  photo:'https://freepikpsd.com/file/2019/10/default-profile-picture-png-1-Transparent-Images.png'
+}]
+
   return(
-    <div className={'h-screen w-full flex items-center justify-center'}>
-      <div className={'h-4/5  w-11/12 grid grid-cols-3 row-end-3'}>
-        <div className={'col-span-3 p-5 flex justify-start self-center ml-5'}>
-          <img src="#" alt="#"/>
-          <div className={'px-4 text-5xl font-bold'}>Account Details</div>
-        </div>
-        <div className={'row-span-3  flex flex-col justify-start'}>
-          <div className={'flex justify-around items-center text-3xl py-10 px-2 bg-white-smoke border-t-black border-t-2'}>
-            <p>Placeholder</p>
-            <img src="#" alt="#"/>
-          </div>
-          <div className={'flex justify-around items-center text-3xl py-10 px-2 bg-white-smoke border-t-black border-t-2'}>
-            <p>Placeholder</p>
-            <img src="#" alt="#"/>
-          </div>
-          <div className={'flex justify-around items-center text-3xl py-10 px-2 bg-white-smoke border-t-black border-t-2'}>
-            <p>Placeholder</p>
-            <img src="#" alt="#"/>
-          </div>
-          <div className={'flex justify-around items-center text-3xl py-10 px-2 bg-white-smoke border-t-black border-t-2'}>
-            <p>Placeholder</p>
-            <img src="#" alt="#"/>
-          </div>
-          <div className={'flex justify-around items-center text-3xl py-10 px-2 bg-white-smoke border-t-black border-t-2'}>
-            <p>Placeholder</p>
-            <img src="#" alt="#"/>
-          </div>
-        </div>
+    <div className="flex flex-col mx-auto justify-center items-center ">
+      <img className="w-1/10 h-1/6 rounded-full mt-10"  src={profilePhoto[0].photo}></img>
+      <h2 className="text-xl font-bold">Olaf Wiśnia</h2>
+      <div className="grid lg:grid-cols-2 m-8 grid-col-1 ">
+        <button className="w-full px-36 py-4 text-center  border-solid border-2  border-gray-400 hover:border-gray-300 active:border-b-black">Moje dane</button>
+        <button className="w-full px-36 py-4 text-center  border-solid border-2  border-gray-400 hover:border-gray-300 active:border-b-black">Histioria moich zleceń</button>
       </div>
     </div>
   )
