3ad4e29a24945ca97383c75bc4b1df060a754d0a
pawelcp <94795184+pawelcp@users.noreply.github.com>
2022-10-14 20:22:03 +0200

    essa123

--- hack_heroes/src/components/Auth/Login.js
+++ hack_heroes/src/components/Auth/Login.js
@@ -5,20 +5,20 @@ const Login = () => {
         < div className="grid grid-cols-1 sm:grid-cols-1 h-screen w-full">
             <div className="bg-white flex flex-col justify-center">
                 <form className="md:max-w-sm sm:max-w-xs drop-shadow-2xl mx-auto bg-whiteGray border-black border-2 p-20 sm:p-14 px-20 rounded-lg">
-                    <h2 className="text-black font-normal text-center text-4xl">Sing in to your account</h2>
+                    <h2 className="text-black font-normal text-center text-4xl">Zaloguj się</h2>
                     <div className="flex flex-col  text-gray-400 py-6">
                         <label className={'font-bold'}>Email</label>
                         <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="email"/>
                     </div>
                     <div className="flex flex-col text-gray-400 py-4">
-                        <label className={'font-bold'}>Password</label>
+                        <label className={'font-bold'}>Hasło</label>
                         <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="text"/>
                     </div>
                     <Link href='/auth/register'>
                     <h1 className="cursor-pointer text-center hover:drop-shadow-md">Nie mam jeszcze konta</h1>
                     </Link>
                     <div className="w-full flex flex-col items-center">
-                      <button className="w-1/2 justify-center drop-shadow-xl m-auto content-center text-white mt-6 py-3 bg-graphite rounded-lg hover:bg-lite-graphite focus:bg-super-lite-graphite">Sing In</button>
+                      <button className="w-1/2 justify-center drop-shadow-xl m-auto content-center text-white mt-6 py-3 bg-graphite rounded-lg hover:bg-lite-graphite focus:bg-super-lite-graphite">Zaloguj</button>
                     </div>
                 </form>
             </div>
--- hack_heroes/src/components/Auth/Register.js
+++ hack_heroes/src/components/Auth/Register.js
@@ -5,9 +5,9 @@ const Register = () => {
     < div className="grid grid-cols-1 sm:grid-cols-1 h-screen w-full">
       <div className="bg-white flex flex-col justify-center">
         <form className="md:w-96 sm:w-80 sm:h-160 drop-shadow-2xl mx-auto bg-whiteGray border-black border-2 p-20 sm:p-10 px-20 rounded-lg">
-          <h2 className="text-black font-normal text-center text-4xl">Register</h2>
+          <h2 className="text-black font-normal text-center text-4xl">Zarejestruj</h2>
           <div className="flex flex-col text-gray-400 py-4">
-            <label className={'font-bold'}>Username</label>
+            <label className={'font-bold'}>Nazwa użytkownika</label>
             <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="text"/>
           </div>
           <div className="flex flex-col  text-gray-400 py-4">
@@ -15,18 +15,18 @@ const Register = () => {
             <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="email"/>
           </div>
           <div className="flex flex-col text-gray-400 py-4">
-            <label className={'font-bold'}>Password</label>
+            <label className={'font-bold'}>Hasło</label>
             <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="text"/>
           </div>
           <div className="flex flex-col text-gray-400 py-4">
-            <label className={'font-bold'}>Repeat Password</label>
+            <label className={'font-bold'}>Powtóż hasło</label>
             <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="text"/>
           </div>
           <Link href='/auth/login'>
             <p className="cursor-pointer text-center hover:drop-shadow-md">Mam juz konto</p>
           </Link>
           <div className="w-full flex flex-col items-center">
-            <button className="w-1/2 justify-center drop-shadow-xl m-auto content-center text-white mt-5  py-3 bg-graphite rounded-lg hover:bg-lite-graphite focus:bg-super-lite-graphite">Register</button>
+            <button className="w-1/2 justify-center drop-shadow-xl m-auto content-center text-white mt-5  py-3 bg-graphite rounded-lg hover:bg-lite-graphite focus:bg-super-lite-graphite">Zarejestruj</button>
           </div>
         </form>
       </div>
