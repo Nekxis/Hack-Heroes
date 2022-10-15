cbc889007e1c557aec00e981c34b169ae917b0cc
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2022-10-15 20:39:47 +0200

    nothing special

--- hack_heroes/src/components/MainSection/MainSection.js
+++ hack_heroes/src/components/MainSection/MainSection.js
@@ -4,8 +4,6 @@ import SectionElement from "./SectionElement/SectionElement";
 export default function MainSection({ orders }) {
 
 
-
-
   return (
     <div className={'w-full grid gap-2 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'}>
       {orders.map((order) => {
