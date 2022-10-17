92f8ac7d2abc2c144e4feb850882ca9cc1a157d7
Nekxis <nexispolska16@gmail.com>
2022-10-17 22:49:18 +0200

    fix

--- hack_heroes/pages/orderDetails/[orderId].js
+++ hack_heroes/pages/orderDetails/[orderId].js
@@ -1,21 +1,25 @@
 import {getDoc, doc} from 'firebase/firestore';
 import { db } from '../../firebase';
+import {useRouter} from "next/router";
 
 
 export default function OrderDetails({ order }) {
-    console.log(order);
+
+  const router = useRouter()
+
     return (
 
       <div className={'flex h-screen w-full'}>
+        <button onClick={async () => { await router.push('/') }} className="fixed drop-shadow-xl text-white px-6 py-4 m-4 bg-graphite rounded-lg hover:bg-lite-graphite focus:bg-super-lite-graphite">Powrót</button>
         <div className={'grid md:grid-cols-3 grid-cols-1 grid-rows-3 md:h-2/3 h-full w-full md:px-16 px-8 self-center sm:pt-8 md:self-center sm:self-start'}>
           <div className={'flex flex-col col-span-1 row-span-1 sm:justify-center md:items-start sm:items-center'}>
-            <img src={`/categories_image/${order.category}.png`} alt="#" className='w-1/2'/>
+            <img src={`/categories_image/${order.category}.png`} alt="#" className='w-1/2 drop-shadow-xl rounded'/>
             <div className={'border-black border-solid border-t-2 mt-2'}>{order.category}</div>
           </div>
           <div className={'flex flex-col md:col-span-2 row-span-1 items-end '}>
             <div className={'flex w-fit justify-end text-right font-bold md:text-6xl text-4xl pl-6 pb-2 border-b-2 border-black'}>{order.title}</div>
-            <div className={'md:w-1/2 w-3/4 flex flex-col justify-end items-end self-end justify-between'}>
-              <div className={'flex justify-between w-5/6 py-2 md:text-2xl'}>
+            <div className={'md:w-2/3 w-3/4 flex flex-col justify-end items-end self-end justify-between'}>
+              <div className={'flex justify-between w-5/6 py-2 md:text-3xl'}>
                 <div>tel. {order.phoneNumber}</div>
                 <div>{order.city}</div>
               </div>
