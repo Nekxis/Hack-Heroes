7130c295c406b206fd094c24e08a47da22e98467
pawelcp <94795184+pawelcp@users.noreply.github.com>
2022-10-17 20:43:58 +0200

    123

--- hack_heroes/src/components/Auth/Login.js
+++ hack_heroes/src/components/Auth/Login.js
@@ -1,20 +1,43 @@
-import {useContext, useState} from "react"
-import  Link  from "next/link"
-import {Context} from "../../Context/Context";
-import {useRouter} from "next/router";
+import Link from "next/link"
+import { useContext, useState } from "react"
+import { Context } from "../../Context/Context";
+import { useRouter } from "next/router";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../../firebase';
+import { db } from '../../../firebase';
+import { setDoc, doc } from 'firebase/firestore';
+import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
 
-
-const Login = () => {
+const Login =  () => {
 
     const [error, setError] = useState(false)
+    const [loginEmail, setLoginEmail] = useState('')
+    const [loginPassword, setLoginPassword] = useState('')
+    const [open, setOpen] = useState(false)
+
     const context = useContext(Context)
     const router = useRouter()
-    const submitHandler = (e) => {
+    const submitHandler = async (e) => {
         e.preventDefault()
-         setError(false)
-         return
+         
+            try{
+                const user = await signInWithEmailAndPassword(
+                    auth,
+                    loginEmail,
+                    loginPassword
+                )
+            }catch (error){
+                console.log(error.message);
+                setError(true)
+                return alert('bledny email lub haslo')
+            }
+        
        }
 
+       const toggle = () => { 
+           setOpen(!open)
+        }
+    
     return(
         < div className="grid grid-cols-1 sm:grid-cols-1 h-screen w-full">
             <div className="bg-white flex flex-col justify-center">
@@ -22,20 +45,23 @@ const Login = () => {
                     <h2 className="text-black font-normal text-center text-4xl">Zaloguj się</h2>
                     <div className="flex flex-col  text-gray-400 py-6">
                         <label className={'font-bold'}>Email</label>
-                        <input className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="email"/>
+                        <input onChange={(e)=>{setLoginEmail(e.target.value)}} className={`rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  ${error ? 'border-red-600' : 'border-gray-400 focus:border-gray-300'}`}/>
                     </div>
-                    <div className="flex flex-col text-gray-400 py-4">
+                    <div className="flex flex-col text-gray-400 py-4 relative">
                         <label className={'font-bold'}>Hasło</label>
-                        <input className={`rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  ${error ? 'border-red-600' : 'border-gray-400 focus:border-gray-300'}`} type="text"/>
+                        <input onChange={(e)=>{setLoginPassword(e.target.value)}} className={`rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  ${error ? 'border-red-600' : 'border-gray-400 focus:border-gray-300'}`} 
+                        type={(open===false)?'password':'text'}>
+                        </input>
+                        <div className="absolute right-2 text-xl bottom-7">
+
+                            {(open===false)?<AiFillEye onClick={toggle}/>:<AiFillEyeInvisible onClick={toggle}/>}
+                        </div>
                     </div>
                     <Link href='/auth/register'>
                     <h1 className="cursor-pointer text-center hover:drop-shadow-md">Nie mam jeszcze konta</h1>
                     </Link>
                     <div className="w-full flex flex-col items-center">
-                      <button onClick={()=> {
-                        context.setButton(false);
-                        router.push('/');
-                      }} className="w-1/2 justify-center drop-shadow-xl m-auto content-center text-white mt-6 py-3 bg-graphite rounded-lg hover:bg-lite-graphite focus:bg-super-lite-graphite">Zaloguj</button>
+                      <button className="w-1/2 justify-center drop-shadow-xl m-auto content-center text-white mt-6 py-3 bg-graphite rounded-lg hover:bg-lite-graphite focus:bg-super-lite-graphite">Zaloguj</button>
                     </div>
                 </form>
             </div>
--- hack_heroes/src/components/Auth/Register.js
+++ hack_heroes/src/components/Auth/Register.js
@@ -6,6 +6,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../../firebase';
 import { db } from '../../../firebase';
 import { setDoc, doc } from 'firebase/firestore';
+
 const Register = () => {
 
   const [password, setPassword] = useState('');
@@ -51,19 +52,19 @@ const Register = () => {
       <div className="bg-white flex flex-col justify-center">
         <form onSubmit={submitHandler} className="md:w-96 sm:w-80 sm:h-160 drop-shadow-2xl mx-auto bg-whiteGray border-gray-400 solid border-2 p-20 sm:p-10 px-20 rounded-lg">
           <h2 className="text-black font-normal text-center text-4xl">Zarejestruj</h2>
-          <div className="flex flex-col text-gray-400 py-4">
+          <div className="flex flex-col text-black py-4">
             <label className={'font-bold'}>Nazwa użytkownika</label>
             <input onChange={(event) => setUserName(event.target.value) } className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="text" />
           </div>
-          <div className="flex flex-col  text-gray-400 py-4">
+          <div className="flex flex-col  text-black py-4">
             <label className={'font-bold'}>Email</label>
             <input onChange={(event) => setRegisterEmail(event.target.value) } className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" type="email" />
           </div>
-          <div className="flex flex-col text-gray-400 py-4">
+          <div className="flex flex-col text-black py-4">
             <label className={'font-bold'}>Hasło</label>
             <input value={password} className="rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2  border-gray-400 focus:border-gray-300" onChange={setPasswordHandler} type="password" />
           </div>
-          <div className="flex flex-col text-gray-400 py-4">
+          <div className="flex flex-col text-black py-4">
             <label className={'font-bold'}>Powtórz hasło</label>
             <input value={passwordAgain} className={`rounded-lg bg-gray-300 mt-2 outline-none p-2 border-solid border-2 ${error ? 'border-red-600' : 'border-gray-400 focus:border-gray-300'}`} onChange={setPasswordAgainHandler} type="password" />
           </div>
@@ -71,10 +72,7 @@ const Register = () => {
             <p className="cursor-pointer text-center hover:drop-shadow-md">Mam juz konto</p>
           </Link>
           <div className="w-full flex flex-col items-center">
-            <button onClick={() => {
-              //
-              //
-            }} className="w-1/2 justify-center drop-shadow-xl m-auto content-center text-white mt-5  py-3 bg-graphite rounded-lg hover:bg-lite-graphite focus:bg-super-lite-graphite">Zarejestruj</button>
+            <button  className="w-1/2 justify-center drop-shadow-xl m-auto content-center text-white mt-5  py-3 bg-graphite rounded-lg hover:bg-lite-graphite focus:bg-super-lite-graphite">Zarejestruj</button>
           </div>
         </form>
       </div>
