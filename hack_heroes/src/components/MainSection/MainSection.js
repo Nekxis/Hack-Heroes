8ab899051920ac427dc08596114e437818512d3c
pawelcp <94795184+pawelcp@users.noreply.github.com>
2022-10-21 23:15:32 +0200

    123

--- hack_heroes/src/components/MainSection/MainSection.js
+++ hack_heroes/src/components/MainSection/MainSection.js
@@ -7,7 +7,7 @@ export default function MainSection({ orders }) {
     <div className={'w-full grid gap-2 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'}>
       {orders.map((order) => {
         return (
-          <SectionElement key={order.id} id={order.id} title={order.title} description={order.description} category={order.category} price={order.price}/>
+          <SectionElement key={order.id} id={order.id} title={order.title} description={order.description} category={order.category} price={order.price} city={order.city}/>
         )
       })}
     </div>
