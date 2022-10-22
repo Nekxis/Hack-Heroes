13cdefd1809e3a132c5b0183af355f2bef5e8e99
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2022-10-22 23:35:53 +0200

    Update Fotter.js

--- hack_heroes/src/components/Footer/Fotter.js
+++ hack_heroes/src/components/Footer/Fotter.js
@@ -1,6 +1,6 @@
 export default function Footer () {
   return (
-    <div className={'sm:hidden h-72  w-full bg-black md:flex justify-around'}>
+    <div className={'sm:hidden h-72 absolute bottom-0 right-0  w-full bg-black md:flex justify-around'}>
       <div className={'md:w-1/5 justify-center items-center h-2/3 flex'}>
         <div className='hidden bg-black w-44 h-14 rounded text-white font-bold lg:flex justify-center items-center text-2xl'>
           <img className='rounded self-center h-full' src='/logo.png' alt='' />
